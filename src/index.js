d72deb38970b67e5b9817324e226da9d1151f3db
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-03-13 22:16:36 -0400

    setup socket.io

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import express from "express"
-console.log("hello world")
+import app from "./app.js"
+import { Server as websockerServer } from "socket.io"
+import http from "http"
+
+const server = http.createServer(app)
+const httpServer = server.listen(3000)
+const io = new websockerServer(httpServer)
+
+console.log("lisent to port 3000")
