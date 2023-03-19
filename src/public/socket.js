e4cd3b1989deb92aff1a9335976f24c4a55e7f38
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-03-18 23:38:07 -0400

    make comments

--- src/public/socket.js
+++ src/public/socket.js
@@ -1,12 +1,12 @@
 const socket = io()
 console.log({socket})
-
+// here we display notes in "front-end"
 export const loadNotesFunction = () => {
     socket.on("loadNotes",(notes) => {
         console.log({notes})
     })
 }
-
+// here we send note to back-end to save it
 export const saveNotes = (title, description) => {
     socket.emit("saveNote",{
         title,
--- src/sockets.js
+++ src/sockets.js
@@ -3,14 +3,14 @@ import Notes from "./models/Notes";
 export default (io) => {
     io.on("connection",(socket)=>{
         console.log("new user connection")
-
+// here we display note's list
         const emitNotes = async () =>{
             const notesList = await Notes.find()
             console.log({notesList})
             socket.emit("loadNotes",notesList)
         }
         emitNotes();
-
+// here we save notes
         socket.on("saveNote", async (infoNote) => {
             const newNote = new Notes({
                 title: infoNote.title,
