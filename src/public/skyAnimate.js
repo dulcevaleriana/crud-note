238c1ed7dae664a5c7ea36b39692594cf5dc5044
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-03-24 16:43:33 -0400

    done add all styles

--- src/public/skyAnimate.js
+++ src/public/skyAnimate.js
@@ -22,16 +22,13 @@ for(let i = 0; i < numBubblesLava; i++){
   blob.classList.add("blob");
   lava.appendChild(blob);
 
+  blob.style.width = `${10+Math.random() * 4}vw`;
+  blob.style.height = `${10+Math.random() * 4}vh`;
+  blob.style.bottom = `${-(60+Math.random() * 200)}%`;
   if(i % 2 !== 0){
     blob.style.left = `${Math.floor(Math.random() * 20) + 1}%`;
-    blob.style.width = `${10+Math.random() * 4}rem`;
-    blob.style.height = `${10+Math.random() * 4}rem`;
-    blob.style.bottom = `${-(60+Math.random() * 200)}%`;
   } else {
     blob.style.right = `${Math.floor(Math.random() * 20) + 1}%`;
-    blob.style.width = `${8+Math.random() * 4}rem`;
-    blob.style.height = `${8+Math.random() * 4}rem`;
-    blob.style.bottom = `${-(50+Math.random() * 200)}%`;
   }
 
   const animation = generateBlobAnimation(index, translateY);
@@ -41,7 +38,7 @@ for(let i = 0; i < numBubblesLava; i++){
   document.head.appendChild(styleSheet);
 
   blob.style.animation = `
-    blob-${index} ${25+Math.random() * 2}s ease-in-out alternate infinite,
+    blob-${index} ${8+Math.random() * 2}s ease-in-out alternate infinite,
     wobble ${2+Math.random() * 2}s ease-in-out alternate infinite
   `;
 }
