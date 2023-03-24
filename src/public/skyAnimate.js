42e9fb90ad343b284fd867bab87d7b8c19dd3ff6
dulce-guzman-azumo <dulce.guzman@azumo.co>
2023-03-24 09:54:58 -0400

    apply new lava background

--- src/public/skyAnimate.js
+++ src/public/skyAnimate.js
@@ -1,25 +1,31 @@
-const bubbles = document.querySelector('.bubbles');
-const numStart = 18
+// const bubbles = document.querySelector('.bubbles');
+// const numStart = 18
 
-// Define una función que agrega la clase 'animate' a las estrellas
+// for(let i = 0; i < numStart; i++){
+//   const bubble = document.createElement("div");
+//   bubble.classList.add('bubble');
 
+//   bubbles.appendChild(bubble)
 
-// Agrega la clase 'animate' a las estrellas después de que se cargue la página
-for(let i = 0; i < numStart; i++){
-  const bubble = document.createElement("div");
-  bubble.classList.add('bubble');
+//   bubble.style.setProperty("--size", `${10+Math.random()*4}rem`);
+//   bubble.style.setProperty("--distance", `${12+Math.random()*4}rem`);
+//   bubble.style.setProperty("--position", `${-5+Math.random()*110}%`);
+//   bubble.style.setProperty("--time", `${3+Math.random()*2}s`);
+//   bubble.style.setProperty("--delay", `${-1*(2+Math.random()*2)}s`);
+// }
 
-  bubbles.appendChild(bubble)
-  // bubble.style.size = `${2+Math.random()*4}rem`;
-  // bubble.style.distance = `${6+Math.random()*4}rem`;
-  // bubble.style.position = `${-5+Math.random()*110}%`;
-  // bubble.style.time = `${2+Math.random()*2}s`;
-  // bubble.style.delay = `${-1*(2+Math.random()*2)}s`;
+const lava = document.querySelector(".lava")
+const numBubblesLava = 10;
 
-  bubble.style.setProperty("--size", `${2+Math.random()*4}rem`);
-  bubble.style.setProperty("--distance", `${6+Math.random()*4}rem`);
-  bubble.style.setProperty("--position", `${-5+Math.random()*110}%`);
-  bubble.style.setProperty("--time", `${2+Math.random()*2}s`);
-  bubble.style.setProperty("--delay", `${-1*(2+Math.random()*2)}s`);
+for(let i = 0; i < numBubblesLava; i++){
+  const blob = document.createElement("div");
+  blob.classList.add("blob");
+  if(i === (numBubblesLava - 2)){
+    blob.classList.add("top");
+  }
+  if(i === (numBubblesLava - 1)){
+    blob.classList.add("bottom");
+  }
+  lava.appendChild(blob);
 }
 
